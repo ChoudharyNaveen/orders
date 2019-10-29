844f8af98c665c43f96556644e4cd1b0044a77da
ChoudharyNaveen <40453335+ChoudharyNaveen@users.noreply.github.com>
2019-10-29 19:46:00 +0530

    Update form-modal.component.ts

--- Alert/src (1)/app (1)/form-modal/form-modal.component.ts
+++ Alert/src (1)/app (1)/form-modal/form-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { NgForm } from '@angular/forms';
+import { OrderServiceService } from '../order-service.service';
 
 @Component({
   selector: 'app-form-modal',
@@ -16,15 +17,18 @@ export class FormModalComponent implements OnInit {
   action:string;
   constructor(
     public dialogRef: MatDialogRef<FormModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public data) {}
+    @Inject(MAT_DIALOG_DATA,) public data,private orderService:OrderServiceService) {}
   ngOnInit() {
     console.log("DATA IS DATA",this.data);
     this.action = this.data.action;
+    if(this.data.action != "Add Order")
+    {
     this.cname = this.data.orderId[0].customerName;
     this.caddress = this.data.orderId[0].customerAddress;
     this.cphone = this.data.orderId[0].customerPhone;
     this.cdate = this.data.orderId[0].orderDueDate;
     this.ctotal = this.data.orderId[0].orderTotal;
+  }
   }
 
   onSubmit(f:NgForm)
@@ -36,7 +40,17 @@ export class FormModalComponent implements OnInit {
       'orderDueDate':this.cdate,
       'orderTotal':this.ctotal
     }
-    console.log("order details",order);
+
+    if(this.data.action != "Add Order")
+    {
+      this.orderService.editOrder(this.data.orderId[0]._id,order).subscribe(order =>{
+        console.log("After Updation",order);
+      })
+    }else{
+      this.orderService.postOrder(order).subscribe(order => {
+        console.log("After Insertion",order);
+      })
+    }
     this.dialogRef.close(order);
   }
 
